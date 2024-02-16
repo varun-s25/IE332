337dc1f5de35387ed9544247e253c1cc94b515dd
rahmahmad <rahmaahmad829@gmail.com>
2024-02-16 00:10:04 -0500

    changes

--- A2/scripts/app.js
+++ A2/scripts/app.js
@@ -1,51 +1,105 @@
+import { createChart } from "./createChart.js";
+import { sortFrequencies, calculateFrequencies } from "./calculateFrequencies.js";
+import { clearTable, populateTable } from "./populateTable.js";
 const resetBtn = document.getElementById('reset-btn');
 const reportBtn = document.getElementById('gen-report-btn');
+const updateBtn = document.getElementById('update-btn');
 const radios = document.getElementsByClassName('radio-inputs');
 const letterRadio = document.getElementById('letter-radio');
 const wordRadio = document.getElementById('word-radio');
 const selectMenu = document.getElementById('select-menu');
+const textArea = document.getElementById('textarea');
 
 let mode = 'letter';
 let category = 'letters';
 
+let chart = createChart([], []);
 const addEventListeners = () => {
     for (let i = 0 ; i < radios.length ; i++) {
         radios[i].addEventListener('click', (e) => {
             if (e.target.defaultValue === 'word') {
                 mode = 'word';
                 selectMenu.disabled = true;
-                selectMenu.classList.add('disable-select');
+                selectMenu.classList.add('hide-select');
             }
             else {
                 mode = 'letter';
                 selectMenu.disabled = false;
-                selectMenu.classList.remove('disable-select');
+                selectMenu.classList.remove('hide-select');
             }
         })
     }
 
     reportBtn.addEventListener('click', () => {
-        console.log(mode);
+        const mode = getMode();
+        const category = getCategory();
+        const text = getText();
+        // send what we have to the server-side script
+        console.log(mode, category, text);
+    })
+
+    updateBtn.addEventListener('click', () => {
+        const mode = getMode();
+        const category = getCategory();
+        const text = getText();
+        if(mode === 'none' || category === '') {
+            alert("Please choose both mode and if applicable, a category.");
+            return;
+        }
+        const frequencies = calculateFrequencies(mode, category, text);
+        const sorted = sortFrequencies(frequencies);
+        const labels = Object.keys(sorted).slice(0, 10);
+        const values = Object.values(sorted).slice(0, 10);
+        chart.destroy();
+        chart = createChart(labels, values);
+        clearTable();
+        populateTable(labels, values);
+    })
+
+
+    resetBtn.addEventListener('click', () => {
+        textArea.value = '';
+        radios[0].checked = false;
+        radios[1].checked = false;
+        selectMenu.value = 'All Letters';
+        selectMenu.disabled = true;
+        selectMenu.classList.add('hide-select');
+        clearTable();
+        if (chart) chart.destroy();
     })
 
     selectMenu.addEventListener('change', () => {
         category = selectMenu.value;
-        console.log(category);
     });
 }
 
 addEventListeners();
 
 
-
-const getMode = (radios) => {
+// returns the chosen mode
+const getMode = () => {
     if (radios[0].checked) {
         return 'letter';
     }
     else if (radios[1].checked) {
-        console.log(selectMenu);
         selectMenu.disabled = true;
         return 'word';
     }
     else return 'none';
+}
+
+// gets the category if the mode is letter
+const getCategory = () => {
+    const mode = getMode();
+    if (mode === 'word') {
+        return undefined;
+    }
+    else {
+        return selectMenu.value;
+    }
+}
+
+// get whatever is in the textarea.
+const getText = () => {
+    return textArea.value;
 }
