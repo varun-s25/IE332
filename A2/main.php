d6f61f7b66e1a9403d4073793179afffa1c7ae30
rahmahmad <rahmaahmad829@gmail.com>
2024-02-22 16:20:50 -0500

    update

--- A2/main.php
+++ A2/main.php
@@ -31,13 +31,11 @@
                             <option value="consonants">All Consonants</option>
                         </select>
                     </div>
-                    <textarea type="text" id="textarea" name="textarea" rows="15" cols="50"></textarea>
+                    <textarea type="text" id="textarea" rows="15" cols="50"></textarea>
                     <div class="btn-container">
-                        <input type="Reset" name="reset-btn"
-                        value="Reset">
                         <button class="primary-btn" id="reset-btn" type="button">Reset</button>
                         <button class="primary-btn" id="update-btn" type="button">Update</button>
-                        <button class="primary-btn" id="gen-report-btn" type="submit">Generate Report</button>
+                        <button class="primary-btn" id="gen-report-btn" name="gen-report'btn" type="button">Generate Report</button>
                     </div>
                 </form>
             </div>
